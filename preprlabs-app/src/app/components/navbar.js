a8c261eb20e38589b13043f9f876db9296f6d8e2
KevinGitSar <kevinsar.97@gmail.com>
2023-11-15 01:36:01 -0500

    Hamburger Navigation Bar (#2).
    - Hamburger Navigation Bar is functional.
    - Navigation bar is responsive from md to lg displays.
    - Hero section almost complete (requires dropdown and tags on search bar).
    - Working on larger display navigation bar.
    - Set list for larger display navigation bar.

--- preprlabs-app/src/app/components/navbar.js
+++ preprlabs-app/src/app/components/navbar.js
@@ -7,35 +7,70 @@ export default function navbar() {
         return setToggle(!toggle);
     }
     return (
-    <>
-    <div className="flex justify-between h-[40px] m-[5px] p-[5px]">
-        <img src="preprlabs-logo.png" className="h-auto" />
-        <button className="h-auto" onClick={() => handleToggle()}>
-            {toggle ? <img src="hamburger-240.png" className="w-[40px] h-auto"/> : <img src="x-mark-240.png" className="w-[40px] h-auto"/>}
-        </button>
-    </div>
-    <div>
-        <div>
-            <div>
-                {/* add Search bar  */}
-            </div>
-            <div>
-                {/* add profile picture name and rank */}
+    <div className="bg-white">
+        {/* Larger Display Navbar */}
+        <div className="hidden lg:block p-5">
+            <div className="flex h-[40px] m-[5px] p-[5px]">
+                <img src="prepr-logo.png" className="w-[100px] h-auto" />
+                <ul className="flex pl-10 mt-auto">
+                    <a href=""><li className="px-5 text-lg text-[#8b8b8b] font-bold hover:text-[#27AE60]">Explore</li></a>
+                    <a href=""><li className="px-5 text-lg text-[#8b8b8b] font-bold hover:text-[#27AE60]">Dashboard</li></a>
+                    <a href=""><li className="px-5 text-lg text-[#8b8b8b] font-bold hover:text-[#27AE60]">My Challenges</li></a>
+                    <a href=""><li className="px-5 text-lg text-[#8b8b8b] font-bold hover:text-[#27AE60]">My Labs</li></a>
+                    <a href=""><li className="px-5 text-lg text-[#8b8b8b] font-bold hover:text-[#27AE60]">My Projects</li></a>
+                    <a href=""><li className="px-5 text-lg text-[#8b8b8b] font-bold hover:text-[#27AE60]">Career Explorer</li></a>
+                </ul>
+                {/* notification bell */}
+
+                {/* chat bubble */}
+
+                {/* question mark */}
+
+                {/* search bar */}
+
+                {/* profile image  */}
+
+                {/* grid + dropdown  */}
             </div>
         </div>
-        <ul>
-            <li className="px-[30px] py-[10px]"><a href="">Explore</a></li>
-            <li className="px-[30px] py-[10px]"><a href="">Dashboard</a></li>
-            <li className="px-[30px] py-[10px]"><a href="">My Labs</a></li>
-            <li className="px-[30px] py-[10px]"><a href="">My Challenges</a></li>
-            <li className="px-[30px] py-[10px]"><a href="">My Projects</a></li>
-            <li className="px-[30px] py-[10px]"><a href="">Browse</a></li>
-            <li className="px-[30px] py-[10px]"><a href="">Account</a></li>
-            <li className="px-[30px] py-[10px]"><a href="">Help Center</a></li>
-            <li className="px-[30px] py-[10px]"><a href="">Log Out</a></li>
-        </ul>
+
+        {/* Smaller Display Navbar */}
+        <div className="lg:hidden p-5 border-b">
+            <div className="flex justify-between h-[40px]">
+                <img src="prepr-logo.png" className="w-[100px] h-auto" />
+                <button className="h-auto" onClick={() => handleToggle()}>
+                    {toggle ? <img src="hamburger-240.png" className="w-[40px] h-auto"/> : <img src="x-mark-240.png" className="w-[40px] h-auto"/>}
+                </button>
+            </div>
+            <div className={toggle ? "fixed w-full inset-x-0 my-5 bg-white transform translate-x-[-100%] transition duration-800 ease-in-out" : "fixed w-full inset-x-0 my-5 bg-white transform translate-x-[0%] transition duration-800 ease-in-out"}>
+                <div className="bg-[#13A859] py-3">
+                    <div className="flex justify-center">
+                        <input type="text" className="w-11/12 mx-auto h-[50px] px-10 rounded text-xl placeholder-[#495057] focus:outline-none" placeholder="Search PreprLabs..."/>
+                    </div>
+                    <div className="mt-8">
+                        <div className="flex w-11/12 mx-auto">
+                            <img src="https://wallpapers.com/images/hd/cool-profile-picture-1ecoo30f26bkr14o.jpg" className="w-[56px] h-auto object-contain"/>
+                            <div className="pl-5">
+                                <p className="text-2xl text-white">Kevin Sar</p>
+                                <p className="text-sm text-white"><span className="align-middle before:content-[url('https://preprlabs.org/assets/images/header/achievement-glyph.svg')]"></span>Learn Rank 0</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <ul>
+                    <a href="/explore"><li className="p-[20px] text-[20px] hover:bg-[#F3F4F8]">Explore</li></a>
+                    <a href=""><li className="p-[20px] text-[20px] hover:bg-[#F3F4F8]">Dashboard</li></a>
+                    <a href="/labs"><li className="p-[20px] text-[20px] hover:bg-[#F3F4F8]">My Labs</li></a>
+                    <a href="/challenges"><li className="p-[20px] text-[20px] hover:bg-[#F3F4F8]">My Challenges</li></a>
+                    <a href="/projects"><li className="p-[20px] text-[20px] hover:bg-[#F3F4F8]">My Projects</li></a>
+                    <a href=""><li className="p-[20px] text-[20px] hover:bg-[#F3F4F8]">Browse</li></a>
+                    <a href=""><li className="p-[20px] text-[20px] hover:bg-[#F3F4F8]">Account</li></a>
+                    <a href=""><li className="p-[20px] text-[20px] hover:bg-[#F3F4F8]">Help Center</li></a>
+                    <a href=""><li className="p-[20px] text-[20px] hover:bg-[#F3F4F8]">Log Out</li></a>
+                </ul>
+            </div>
+        </div>
     </div>
-    </>
     )
   }
   
--- preprlabs-app/src/app/explore/page.js
+++ preprlabs-app/src/app/explore/page.js
@@ -1,11 +1,49 @@
+"use client"; // This is a client component
+import {useState} from "react";
 import Navbar from "../components/navbar"
 
 export default function Home() {
+    let [heroToggle, setHeroToggle] = useState(true);
+    const handleHeroToggle = () => {
+        return setHeroToggle(!heroToggle);
+    }
     return (
-      <div>
+    <>
         <Navbar />
-        {/* <p>Hello Explore</p> */}
-      </div>
+        {/* Hero Section? */}
+        <div className={heroToggle ? 'my-10 p-5 bg-gradient-to-r from-[#D5FFC6] to-[#7BD1F2] text-center' : 'hidden'}>
+            
+                <button className="h-auto float-right" onClick={() => handleHeroToggle()}>
+                    <img src="x-mark-240.png" className="w-[40px] h-auto"/>
+                </button>
+
+                <h1 className="mt-20 text-5xl font-bold">Explore Your Interests</h1>
+                <p className="mt-5 font-medium">We'll recommend content based on your interests we've listed here.</p>
+                <p className="mt-1 font-medium">Feel free to add or remove topics to customize your experience !</p>
+
+                <div className="flex justify-center w-11/12 md:w-8/12 lg:w-1/2 2xl:w-5/12 my-20 mx-auto">
+                        <img src="search-240.png" className="bg-white p-4 w-[50px] h-auto border-r rounded-l-lg"/>
+                    
+                        {/* Add dropdownlist and tags into search bar */}
+                        <input type="text" className="pl-5 rounded-r-lg grow focus:outline-none" placeholder="Search Skills" />
+                </div>
+
+                <h2 className="font-bold text-3xl">Click an interest to narrow your recommendations.</h2>
+
+                <div className="mt-3 mb-20 w-11/12 md:w-8/12 lg:w-1/2 2xl:w-5/12 mx-auto">
+                    <button className="bg-white border rounded-lg p-1 my-2 mx-3">Presentation</button>
+                    <button className="bg-white border rounded-lg p-1 my-2 mx-3">Neural Intelligence</button>
+                    <button className="bg-white border rounded-lg p-1 my-2 mx-3">Wellness</button>
+                    <button className="bg-white border rounded-lg p-1 my-2 mx-3">Project Leadership</button>
+                    <button className="bg-white border rounded-lg p-1 my-2 mx-3">Planning</button>
+                    <button className="bg-white border rounded-lg p-1 my-2 mx-3">STAR</button>
+                    <button className="bg-white border rounded-lg p-1 my-2 mx-3">GitLab</button>
+                    <button className="bg-white border rounded-lg p-1 my-2 mx-3">Matplotlib</button>
+                    <button className="bg-white border rounded-lg p-1 my-2 mx-3">Presentation Solutions</button>
+                    <button className="bg-white border rounded-lg p-1 my-2 mx-3">Digital Production</button>
+                </div>
+        </div>
+    </>
     )
   }
   
